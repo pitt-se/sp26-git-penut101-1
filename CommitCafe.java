7fd0a505c967c673e735b0ae1294df3e886928bc
Aiden Nemeroff <penut101@outlook.com>
2026-04-06 13:34:24 -0400

    Added brew method with other changes

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,19 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Aiden"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String nickname = "Head Barista";
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println(nickname + " " + barista + " brewed a cup of " + drink + ". Total cups: " + cups);
         // STUDENT_TODO_2B: Add a second brew-related improvement
+        if (drink.equalsIgnoreCase("Espresso")){
+            System.out.println("Strong Coffee!");
+        }
     }
 
     public static void printSummary() {
